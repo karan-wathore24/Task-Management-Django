8aecf7020205ed78f0f9514952ed25286715498d
karan-wathore24 <karanwathore2002@gmail.com>
2026-01-23 02:11:55 +0530

    Clean setup for Render deploymet

--- todo_list/todo_list/settings.py
+++ todo_list/todo_list/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,7 +84,10 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+if os.environ.get('DATABASE_URL'):
+    DATABASES['default']=dj_database_url.parse(
+        os.environ.get('DATABASE_URL')
+    )
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
