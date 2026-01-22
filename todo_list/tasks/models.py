50406e73d9e1bb6d699913ade1117223cf6b0d04
karan-wathore24 <karanwathore2002@gmail.com>
2026-01-23 02:29:27 +0530

    Clean setup for Render deploymet

--- todo_list/tasks/models.py
+++ todo_list/tasks/models.py
@@ -4,11 +4,11 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Task(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField(blank=True)
-    completed = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200,blank=True,null=True)
+    description = models.TextField(blank=True,null=True)
+    completed = models.BooleanField(default=False,blank=True,null=True)
+    created_at = models.DateTimeField(auto_now_add=True,blank=True,null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE,blank=True,null=True)
 
     def __str__(self):
         return self.title
