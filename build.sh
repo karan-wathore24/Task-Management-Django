3916fd0237969c136c97ac86c3ec43773ec5a6a5
karan-wathore24 <karanwathore2002@gmail.com>
2026-01-23 01:52:36 +0530

    Clean setup for Render deploymet

--- build.sh
+++ build.sh
@@ -2,12 +2,13 @@
 # Exit on error
 set -o errexit
 
-# Install dependencies from the root directory
+# 1. Install dependencies from the root directory
 pip install -r requirements.txt
 
-# Move into the project directory where manage.py is located
+# 2. Go into the folder where manage.py is located
 cd todo_list
 
-# Run your Django commands
+# 3. Run the Django commands from inside that folder
 python manage.py collectstatic --no-input
+python manage.py makemigrations
 python manage.py migrate
